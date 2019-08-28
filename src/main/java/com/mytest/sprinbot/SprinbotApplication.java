9732cc694d597502febb9ffd208f4787dda43f70
zhouheng <zhouheng@weilaihui.com>
2019-08-28 15:16:54 +0800

    commont

--- src/main/java/com/mytest/sprinbot/SprinbotApplication.java
+++ src/main/java/com/mytest/sprinbot/SprinbotApplication.java
@@ -1,14 +1,11 @@
 package com.mytest.sprinbot;
 
-import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
-import com.alicp.jetcache.anno.config.EnableMethodCache;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableCreateCacheAnnotation
-@EnableMethodCache(basePackages = "com.mytest.sprinbot.service")
+
 @MapperScan("com.mytest.sprinbot.mapper")
 public class SprinbotApplication {
 
